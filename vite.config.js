1e1c9b3f62c4ed9bc1e4d6eb8ecf8b3fa862a87a
Arlind Jashari <jashariarlind@gmail.com>
2026-02-06 15:05:49 +0100

    Configure clean URLs and update internal links

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,33 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
+const htmlExtFallback = () => {
+  return {
+    name: 'html-ext-fallback',
+    configureServer(server) {
+      server.middlewares.use((req, res, next) => {
+        if (req.url.length > 1 && req.url.indexOf('.') === -1) {
+          req.url += '.html';
+        }
+        next();
+      });
+    }
+  };
+};
+
 export default defineConfig({
+  plugins: [htmlExtFallback()],
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
         about: resolve(__dirname, 'about.html'),
+        what_we_do: resolve(__dirname, 'what-we-do.html'),
         projects: resolve(__dirname, 'projects.html'),
+        project_detail: resolve(__dirname, 'project-detail.html'),
+        careers: resolve(__dirname, 'careers.html'),
+        news: resolve(__dirname, 'news.html'),
+        news_detail: resolve(__dirname, 'news-detail.html'),
         contact: resolve(__dirname, 'contact.html'),
       },
       output: {
